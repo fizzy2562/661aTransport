c36abf56e3e4184e4955d96cc9be0c7a972e54a6
Ciaran Fitzgerald <fizzy2562@hotmail.com>
2025-06-13 15:44:30 +0200

    661ACode.py

--- 661ACode.py
+++ 661ACode.py
@@ -358,6 +358,9 @@ def dashboard():
     return render_template_string(html, stops=all_departures, now=datetime.now(brussels).strftime('%H:%M:%S'))
 
 if __name__ == '__main__':
-   import os
-port = int(os.environ.get("PORT", 5050))
-app.run(host="0.0.0.0", port=port)
+    
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5050))
+    app.run(host='0.0.0.0', port=port, debug=True)
